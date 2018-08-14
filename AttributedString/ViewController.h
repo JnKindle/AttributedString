c25446a846f5aed47bf241c33d6bf61d0c28fed1
JnKindle <Jn_never2013@163.com>
2018-08-14 16:25:27 +0800

    Initial Commit

---
+++ AttributedString/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AttributedString
+//
+//  Created by Jn_Kindle on 2018/8/14.
+//  Copyright © 2018年 JnKindle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AttributedString/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  AttributedString
+//
+//  Created by Jn_Kindle on 2018/8/14.
+//  Copyright © 2018年 JnKindle. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
